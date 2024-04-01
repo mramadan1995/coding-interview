d1e4d625edf60f645a324db035ef4cafb3b199ef
mramadan19 <mramadan@unitedofoq.com>
2024-04-01 16:33:32 +0200

    fix round number to 100

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,5 +4,5 @@ fun main() {
     val playerA = Player { Move.ROCK }
     val playerB = Player { Move.entries.toTypedArray().random() }
     val game = Game(playerA,playerB)
-    game.playRounds(200)
+    game.playRounds(100)
 }
